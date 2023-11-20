f5a2e8f26540654360ed28286a2dc35cf356c6e7
pvinere <123131941+pvinere@users.noreply.github.com>
2023-11-20 12:46:41 +0200

    Repair vercel

--- emergency-app/src/environments/environment.prod.ts
+++ emergency-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDoa6LVHx4FSZgXxy4LmjaYeSujZrynnjw",
+    authDomain: "emergency-app-c3671.firebaseapp.com",
+    projectId: "emergency-app-c3671",
+    storageBucket: "emergency-app-c3671.appspot.com",
+    messagingSenderId: "759438804028",
+    appId: "1:759438804028:web:969bf2f164841ebba06f39",
+    measurementId: "G-EZTDQSX18C"
+  }
 };
