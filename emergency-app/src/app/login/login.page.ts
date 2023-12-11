d105a864cc7c1a7d56de41c8d4baac6e6cbca398
pvinere <123131941+pvinere@users.noreply.github.com>
2023-12-11 16:53:09 +0200

    Add login function
    Without email verification yet...

--- emergency-app/src/app/login/login.page.ts
+++ emergency-app/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../shared/authentication-service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    public authService: AuthenticationService,
+    public router: Router
+  ) {}
+  ngOnInit() {}
+  logIn(email: any, password: any) {
+    this.authService
+      .SignIn(email.value, password.value)
+      .then((): any => {
+        if (this.authService.isEmailVerified) {
+          this.router.navigate(['/tabs/home']);
+        } else {
+          window.alert('Email is not verified');
+          return false;
+        }
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
   }
-
 }
