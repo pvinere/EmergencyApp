c6cf64bfb0d3c2c2b92fa515c7c7d65010cbab9b
pvinere <123131941+pvinere@users.noreply.github.com>
2023-12-12 13:00:14 +0200

    Add password and confirm password validators

--- emergency-app/src/app/register-page/register-page.page.ts
+++ emergency-app/src/app/register-page/register-page.page.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../shared/authentication-service';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.page.html',
   styleUrls: ['./register-page.page.scss'],
 })
 export class RegisterPagePage implements OnInit {
+  
+form: FormGroup;
 
   constructor(
     public authService: AuthenticationService,
     public router: Router
-  ) {}
+    
+  ) {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    {
+      validators : this.passwordMatchValidator
+    }
+    );
+    
+  }
 
   ngOnInit() {
+  
   }
   signUp(email: any, password: any) {
     this.authService
@@ -24,6 +44,10 @@ export class RegisterPagePage implements OnInit {
       .catch((error) => {
         window.alert(error.message);
       });
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    return control.get('password')?.value === control.get('confirmPassword')?.value ? null : { mismatch: true };
   }
 
 }
