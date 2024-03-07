7a62c52efb79c14d526d94c89dd7233bb49bd675
pvinere <123131941+pvinere@users.noreply.github.com>
2024-03-07 21:00:43 +0200

    Add Dark Mode button to settings
    Only for web

--- emergency-app/src/app/tab_details/tab4.page.ts
+++ emergency-app/src/app/tab_details/tab4.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab4.page.html',
   styleUrls: ['tab4.page.scss']
 })
-export class Tab4Page {
+export class Tab4Page implements OnInit{
 
   constructor() {}
+  ngOnInit(): void {
+    this.checkAppMode();
+  }
+
+  darkMode = false;
+
+  async checkAppMode() {
+  
+    const checkIsDarkMode = localStorage.getItem('darkModeActivated');
+    console.log(checkIsDarkMode);
+    checkIsDarkMode == 'true'
+      ? (this.darkMode = true)
+      : (this.darkMode = false);
+    document.body.classList.toggle('dark', this.darkMode);
+  }
+  toggleDarkMode()
+  {
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark',this.darkMode);
+    if(this.darkMode){
+      localStorage.setItem('darkModeActivated','true');
+    }
+    else
+    {
+      localStorage.setItem('darkModeActivated','false');
+    }
+  }
 
 }
